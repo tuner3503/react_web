5091e40ad92e800b9d867db2fd66bd9ed0028616
Tuner9696 <1030zzzz@gmail.com>
2022-08-15 00:31:37 +0900

    a

--- src/Listitems.js
+++ src/Listitems.js
@@ -6,15 +6,15 @@ import classes from "./Listitems.module.css";
 
 
 export default function Listitems(props) {
-   const {
-      iframeSource = '<iframe height=300 width=300 src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=tuner3503-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07B7NXV4R&linkId=fccb15e38a017dc265ed4e258756b3f4" ></iframe>                 '
-    } = props;
+   // const {
+   //    iframeSource = '<iframe height=300 width=300 src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=tuner3503-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07B7NXV4R&linkId=fccb15e38a017dc265ed4e258756b3f4" ></iframe>                 '
+   //  } = props;
  return (
 
     <div>     
     <p>listItems</p>
     <br/>                              //追加　改行のhtmlタブ
-    <div  className={classes.test} dangerouslySetInnerHTML={{__html: iframeSource}}></div>;
+    {/* <div  className={classes.test} dangerouslySetInnerHTML={{__html: iframeSource}}></div>; */}
     
     </div>   
  
